4b4a3af2b308cefe96050715a8357be5866a7f0d
sac-hub <si.campbell1@gmail.com>
2025-07-16 15:39:37 +0100

    From Eclipse No3

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -18,7 +18,7 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys UPDATED 1234567");
+		System.out.println("Hello Guys UPDATED 12345678");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
