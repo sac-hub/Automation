eb19a27935f93ead1228a46862537f4d87deb3a3
sac-hub <si.campbell1@gmail.com>
2025-07-20 16:42:05 +0100

    Will this work AZURE

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -18,7 +18,7 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys UPDATED 123456789101112");
+		System.out.println("Hello Guys UPDATED 12345678910111213");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
