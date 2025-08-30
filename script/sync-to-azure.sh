0d1aa6b4ffed1cc035ac5835bf2518273b58e45d
sac-hub <si.campbell1@gmail.com>
2025-08-30 15:36:39 +0100

    unshallow

--- script/sync-to-azure.sh
+++ script/sync-to-azure.sh
@@ -49,7 +49,7 @@ rm -rf Automation/.git
 #git rebase --root origin/master**
 
 # Fetch the changes from Azure DevOps to ensure we have latest
-git fetch --unshallow
+git fetch #--unshallow
 
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
 git pull https://$AZURE_NAME:$AZURE_PAT@dev.azure.com/$AZURE_ORG/My%20first%20DevOps/_git/Automation.git
