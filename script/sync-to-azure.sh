ec63f944e251fb7c576cfcf6c92d1831dab581de
sac-hub <si.campbell1@gmail.com>
2025-08-30 15:40:47 +0100

    echo added

--- script/sync-to-azure.sh
+++ script/sync-to-azure.sh
@@ -47,20 +47,20 @@ rm -rf Automation/.git
 
 #git fetch origin
 #git rebase --root origin/master**
-
+echo "1"
 # Fetch the changes from Azure DevOps to ensure we have latest
 git fetch #--unshallow
-
+echo "2"
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
 git pull https://$AZURE_NAME:$AZURE_PAT@dev.azure.com/$AZURE_ORG/My%20first%20DevOps/_git/Automation.git
-
+echo "3"
 # Set Git user identity
 git config --global user.name "$AZURE_NAME"
 git config --global user.email "$AZURE_EMAIL"
-
+echo "4"
 # Add all changes into stage, commit, and push to Azure DevOps dd
 git add .
 git commit -m "Sync from GitHub to Azure DevOps"
 git push --force https://$AZURE_NAME:$AZURE_PAT@dev.azure.com/$AZURE_ORG/My%20first%20DevOps/_git/Automation.git
-
+echo "5"
 echo "✅ Sync complete"
