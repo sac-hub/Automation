4b7182fcb72fb7afd83a5ddb1c358c2aecce608a
sac-hub <si.campbell1@gmail.com>
2025-08-31 11:22:59 +0100

    new Update

--- script/sync-to-azure2.sh
+++ script/sync-to-azure2.sh
@@ -34,7 +34,7 @@ echo "Bats version $(bats --version) installed successfully."
 # Confirm installation
 echo "Bats version $(bats --version) installed successfully."
 
-# === Configuration =======================================================================================
+# === Configuration ===
 
 AZURE_PAT=$AZURE_PAT
 AZURE_NAME=$AZURE_NAME
@@ -44,12 +44,14 @@ GITHUB_URL="https://github.com/sac-hub/Automation.git"
 REMOTE_URL="https://$AZURE_NAME:$AZURE_PAT@dev.azure.com/$AZURE_ORG/My%20first%20DevOps/_git/Automation.git"
 
 # === Derived ===++
+
+rm -rf Automation/.git
+
+#git fetch origin
+#git rebase --root origin/master**
 echo "1"
-git remote remove origin || true
-git remote add origin "$GITHUB_URL"
-echo "2"
-echo "Fetching from GitHub..."
-git fetch origin master
+# Fetch the changes from Azure DevOps to ensure we have latest
+git fetch $GITHUB_URL
 echo "2"
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
 
